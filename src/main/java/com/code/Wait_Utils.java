1d8e896b10b090c05b7de33b2b82d8c564d53e9e
ajay_bhave <bhaveajay007@gmail.com>
2022-08-19 21:54:13 +0530

    Keyboard actions

--- src/main/java/com/code/Wait_Utils.java
+++ src/main/java/com/code/Wait_Utils.java
@@ -1,7 +1,56 @@
 package com.code;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Wait_Utils {
-	
-	
+
+	static WebDriver driver;
+
+	// wait for element to present in DOM
+	public WebElement waitForElementPresence(By locator, int timeOut) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut));
+		return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+
+	}
+
+	// returns WebElement with interval time(polling time)
+	public  WebElement waitForElementPresenceWithInterval(By locator, int timeOut, long intervalTime) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut), Duration.ofMillis(intervalTime));
+		return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+	}
+
+	// checks for visiblity and presence of element on the DOM
+	public WebElement visibilityOfElement(By Locator, int timeOut) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut));
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(Locator));
+	}
+
+	// get list of webelement once they are visible
+	public  List<WebElement> getVisibleElements(WebDriver driver, By locator, int timeOut) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut));
+		return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+	}
+
+	// enter text to the element once it is visible
+	public void safeType(By locator, int timeOut, String value) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut));
+		WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+		element.sendKeys(value);
+
+	}
+
+	// click on the element once it is visible
+	public void safeClick(By locator, int timeOut) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut));
+		WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+		element.click();
+	}
 
 }
